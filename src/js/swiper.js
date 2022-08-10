e17f812bc2b3a39ef22684571da8061c02c6f5ec
VadymZ1978 <106116386+VadymZ1978@users.noreply.github.com>
2022-08-10 22:37:06 +0300

    Update swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,41 @@
 $(function () {
   $('.swiper-wrapper').slick({
+<<<<<<< Updated upstream
     Infinity: true,
+=======
+>>>>>>> Stashed changes
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     speed: 300,
+<<<<<<< Updated upstream
+=======
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+>>>>>>> Stashed changes
   });
 });
