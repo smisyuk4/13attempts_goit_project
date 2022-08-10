8270b551a76b9acaeaa034452c76006902e268bb
VadymZ1978 <106116386+VadymZ1978@users.noreply.github.com>
2022-08-10 17:58:23 +0300

    Update swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,9 @@
-var menu = ['North Shore', 'South Shore', 'West Shore', 'East Shore']
-
-window.onload = function() {
-  var mySwiper = new Swiper('.swiper-container', {
-    // loop: true,
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function(index, className) {
-        return '\
-          <div class="box ' + className + '">\
-          <div class="bigNumber">' + (index < 10 ? '0' + (index + 1) : (index + 1)) + '</div>\
-          <div class="text">' + (menu[index]) + '</div>\
-          </div>';
-      },
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  })
-};
+$(function () {
+  $('.swiper-wrapper').slick({
+    Infinity: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    speed: 300,
+  });
+});
